384827a2e35e4a51ced26114025e2859a08bdf87
HikaroRibeiro <hhakawama@gmail.com>
2020-06-28 08:53:59 -0300

    sparkle: this is the end of the challenge

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,5 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import transactionRouter from '../routes/transaction.routes';
+
+interface TransactionDTO{
+  title: string,
+  value: number,
+  type: 'income' | 'outcome'
+}
 
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
@@ -8,9 +15,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title,value,type}:TransactionDTO): Transaction {
+
+    const {total} = this.transactionsRepository.getBalance();
+
+    if(!["outcome","income"].includes(type)){
+      throw new Error(`This value ${type} is invalid!`);
+    }
+
+    if(type === 'outcome' && total < value){
+      throw new Error('You do not have enough balance');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type
+    })
+    return transaction;
   }
+  
 }
 
 export default CreateTransactionService;
